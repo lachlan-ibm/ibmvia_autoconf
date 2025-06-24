4abb7afe6de1576f49ebd40b8036be3a643bdcc1
Lachlan Gleeson <lgleeson@au1.ibm.com>
2025-06-24 13:12:52 +1000

    syntax error

--- src/ibmvia_autoconf/webseal.py
+++ src/ibmvia_autoconf/webseal.py
@@ -136,8 +136,7 @@ class WEB_Configurator(object):
         for fc in fed_config:
             #Convert federation name to uuid
             fed_cfg = copy.deepcopy(fc)
-            fed_cfg['federation_id'] = optional_list(filter_list('name', fc.name, federations))[0].get("id", "-1")
-            del fed_cfg['name']
+            fed_cfg['federation_id'] = optional_list(filter_list('name', fed_cfg.pop('name'), federations))[0].get("id", "-1")
             prefix_keys(fed_cfg, "runtime", "runtime_")
             #Run the wizard
             rsp = self.web.reverse_proxy.configure_fed(proxy_id, **fed_cfg)
