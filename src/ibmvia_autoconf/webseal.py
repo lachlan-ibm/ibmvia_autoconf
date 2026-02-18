50361028c133cd66279a4953bee4e3a26ab62fe3
Lachlan Gleeson <lgleeson@au1.ibm.com>
2026-02-18 16:41:54 +1000

    Update doc for embedded ldap secret

--- src/ibmvia_autoconf/webseal.py
+++ src/ibmvia_autoconf/webseal.py
@@ -712,7 +712,6 @@ class WEB_Configurator(object):
             _logger.info("RTE already configured, skipping configuration.")
             if runtime.embedded_root_password:
                 self._update_internal_ldap_secret(runtime.embedded_root_password)
-            return
         else: #Not available, so we can configure it
             config = {"ps_mode": runtime.policy_server,
                     "user_registry": runtime.user_registry,
